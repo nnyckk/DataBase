0511d7bd4e0f1b0e66f13c87bb4ea37520fb27e4
nnyckk <neagoe.nick@gmail.com>
2025-05-09 11:58:40 +0200

    New Folder

--- SQL2/SQL2_Competence_Check.sql
+++ SQL2/SQL2_Competence_Check.sql
@@ -6,6 +6,23 @@ create table
 );
 
 
+create table countries (
+    country_id char(2) constraint country_primary_key primary key,
+    country_name varchar(40),
+    region_id number(11),
+    Foreign Key (region_id) References regions(region_id)
+);
+
+
+create table locations (
+    location_id number(4) constraint location_primary_key primary key,
+    street_address varchar(40),
+    postal_code varchar(12),
+    city varchar(30),
+    state_province varchar(25),
+    country_id char(2),
+    Foreign Key (country_id) References countries(country_id)
+);
 
 
 
