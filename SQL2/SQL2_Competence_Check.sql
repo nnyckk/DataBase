31ad68f5870a4071cd13cfc28a0054eda855aba8
nnyckk <neagoe.nick@gmail.com>
2025-05-12 09:00:43 +0200

    Tables inserted

--- SQL2/SQL2_Competence_Check.sql
+++ SQL2/SQL2_Competence_Check.sql
@@ -24,8 +24,50 @@ create table locations (
     Foreign Key (country_id) References countries(country_id)
 );
 
- d
+create table departments (
+    department_id number(4) constraint department_primary_key primary key,
+    department_name varchar(30),
+    manager_id number(6),
+    location_id number(4),
+    Foreign Key (location_id) References locations(location_id)
+);
 
 
+create table jobs (
+    job_id varchar(20) constraint job_primary_key primary key,
+    job_title varchar(35),
+    min_salary number(6),
+    max_salary number(6)
+);
+
+
+create table employees (
+    employee_id number(6) constraint employee_primary_key primary key,
+    first_name varchar(20),
+    last_name varchar(20),
+    email varchar(25),
+    phone_number varchar(20),
+    hire_date date,
+    job_id varchar(10),
+    salary decimal(8,2),
+    commission_pct decimal(2,2),
+    manager_id number(6),
+    department_id number(4),
+    Foreign Key (job_id) References jobs(job_id),
+    Foreign Key (manager_id) References employees(employee_id),
+    Foreign Key (department_id) References departments(department_id)
+);
+
+
+create table job_history (
+    employee_id number(6),
+    start_date date constraint job_history_primary_key primary key,
+    end_date date,
+    job_id varchar(10),
+    department_id number(10),
+    Foreign Key (employee_id) References employees(employee_id),
+    Foreign Key (job_id) References jobs(job_id),
+    Foreign Key (department_id) References departments(department_id)
+)
 
 
